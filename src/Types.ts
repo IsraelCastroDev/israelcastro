790ac0f0de5a8f78309e6f4a676fb82d36997bb1
Isreal Castro <israelcastro.dev@gmail.com>
2024-04-08 14:38:19 -0500

    Ajustando detalles del menu de navegación

---
+++ src/Types.ts
@@ -0,0 +1,5 @@
+export type MenuItems = {
+  name: string;
+  href: string;
+  className: string
+};
---
+++ src/data/menu.ts
@@ -0,0 +1,29 @@
+import type { MenuItems } from "@/Types";
+
+export const menu: MenuItems[] = [
+  {
+    name: "Inicio",
+    href: "#Hero",
+    className: "inicio",
+  },
+  {
+    name: "Proyectos",
+    href: "#Proyectos",
+    className: "proyectos",
+  },
+  {
+    name: "Habilidades",
+    href: "#Habilidades",
+    className: "habilidades",
+  },
+  {
+    name: "Sobre mi",
+    href: "#SobreMi",
+    className: "sobre-mi",
+  },
+  {
+    name: "Contacto",
+    href: "#Contacto",
+    className: "contacto",
+  },
+];
