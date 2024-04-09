54fd53cc0b17ef57833b4c23d91646bb69acd5f3
Isreal Castro <israelcastro.dev@gmail.com>
2024-04-09 08:01:41 -0500

    Creando la data de habilidades

---
+++ src/data/habilidades.ts
@@ -0,0 +1,36 @@
+import type { Habilidades } from "@/Types";
+
+export const habilidades: Habilidades[] = [
+  {
+    name: "HTML",
+    img: "/icons/HTML.svg",
+  },
+  {
+    name: "CSS",
+    img: "/icons/CSS.svg",
+  },
+  {
+    name: "TailwindCSS",
+    img: "/icons/TailwindCSS.svg",
+  },
+  {
+    name: "JavaScript",
+    img: "/icons/JavaScript.svg",
+  },
+  {
+    name: "TypeScript",
+    img: "/icons/TypeScript.svg",
+  },
+  {
+    name: "React",
+    img: "/icons/React.svg",
+  },
+  {
+    name: "Astro",
+    img: "/icons/Astro.svg",
+  },
+  {
+    name: "Git",
+    img: "/icons/Git.svg",
+  },
+];
