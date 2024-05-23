871b4a2142c79a5512e904d0470757b8deda74a8
Isreal Castro <israelcastro.dev@gmail.com>
2024-05-23 11:27:30 -0500

    Agregando un data de un proyecto

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -2,12 +2,12 @@ import type { Proyecto } from "@/Types";
 
 export const proyectos: Proyecto[] = [
   {
-    name: "Cotizador de Criptomonedas",
+    name: "Librería virtual",
     description:
-      "Proyecto hecho con React y TailwindCSS, consumiendo una API de criptomonedas en tiempo real para obtener la cotización actual de cada criptomoneda.",
-    img: "/images/cripto.webp",
-    url: "https://cripto-react-eight.vercel.app/",
-    technologies: ["React", "API", "TailwindCSS"],
+      "Hecha en React, usando TailwindCSS para los estilos, custom hooks para separar la lógica de los componentes y useReducer con Context API",
+    img: "/images/libreria-react.webp",
+    url: "https://libreria-virtual-react.vercel.app/",
+    technologies: ["React", "TailwindCSS"],
   },
   {
     name: "Tienda de Guitarras",
@@ -18,35 +18,11 @@ export const proyectos: Proyecto[] = [
     technologies: ["React", "CSS", "TypeScript"],
   },
   {
-    name: "Calculadora de Propinas",
-    description:
-      "Proyecto hecho con React y TypeScript, usando TailwindCSS para los estilos, con lógica para calcular propinas a base de un porcentaje elegido por el usuario.",
-    img: "/images/calculador-propinas.webp",
-    url: "https://ubiquitous-cannoli-070af7.netlify.app/",
-    technologies: ["React", "TailwindCSS", "TypeScript"],
-  },
-  {
     name: "Cotizador de Criptomonedas",
     description:
       "Proyecto hecho con React y TailwindCSS, consumiendo una API de criptomonedas en tiempo real para obtener la cotización actual de cada criptomoneda.",
     img: "/images/cripto.webp",
     url: "https://cripto-react-eight.vercel.app/",
     technologies: ["React", "API", "TailwindCSS"],
-  },
-  {
-    name: "Tienda de Guitarras",
-    description:
-      "Proyecto hecho con React y TypeScript, esta vez usando CSS para los estilos, usé LocalStorage y custom kooks para separar la lógica de los componentes.",
-    img: "/images/tienda-guitarras.webp",
-    url: "https://magnificent-salmiakki-3ed837.netlify.app/",
-    technologies: ["React", "CSS", "TypeScript"],
-  },
-  {
-    name: "Calculadora de Propinas",
-    description:
-      "Proyecto hecho con React y TypeScript, usando TailwindCSS para los estilos, con lógica para calcular propinas a base de un porcentaje elegido por el usuario.",
-    img: "/images/calculador-propinas.webp",
-    url: "https://ubiquitous-cannoli-070af7.netlify.app/",
-    technologies: ["React", "TailwindCSS", "TypeScript"],
   },
 ];
