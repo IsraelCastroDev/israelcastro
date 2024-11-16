cb4f2fba5a8a268e3cdcc5300f4ff1d6f97842f4
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-16 13:25:09 -0500

    agregando nuevo proyecto

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -2,13 +2,39 @@ import type { Proyecto } from "@/Types";
 
 export const proyectos: Proyecto[] = [
   {
+    name: "Super Quiz",
+    description:
+      "Aplicación web para crear, jugar y gestionar cuestionarios. Desarrollada con tecnologías modernas, incluye un backend con Node y Express, usando MongoDB como base de datos.",
+    img: "/images/super-quiz.webp",
+    url: "https://super-quiz-five.vercel.app/",
+    technologies: [
+      "React",
+      "TypeScript",
+      "TailwindCSS",
+      "Express",
+      "MongoDB",
+      "ReactQuery",
+    ],
+  },
+  {
     name: "App Flashvideo",
     description:
-      "App de películas, consume la API de TheMovieDB usando Tanstack Query React, TailwindCSS y TypeScript. Usé custom hooks para reutilizar los queries.",
+      "App de películas, consume la API de TheMovieDB usando Tanstack Query React, TailwindCSS y TypeScript. Usé custom hooks para reutilizar los queries y separar lógica de componentes.",
     img: "/images/bg-flashvideo.webp",
     url: "https://flashvideo.vercel.app/",
     technologies: ["React", "CSS", "TypeScript", "API", "ReactQuery"],
   },
+  {
+    name: "Tienda de Guitarras",
+    description:
+      "Proyecto hecho con React y TypeScript, esta vez usando CSS para los estilos, usé LocalStorage para la persistencia y custom kooks para separar la lógica de los componentes.",
+    img: "/images/tienda-guitarras.webp",
+    url: "https://magnificent-salmiakki-3ed837.netlify.app/",
+    technologies: ["React", "CSS", "TypeScript"],
+  },
+];
+
+/*
   {
     name: "Librería virtual",
     description:
@@ -17,12 +43,4 @@ export const proyectos: Proyecto[] = [
     url: "https://libreria-virtual-react.vercel.app/",
     technologies: ["React", "TailwindCSS"],
   },
-  {
-    name: "Tienda de Guitarras",
-    description:
-      "Proyecto hecho con React y TypeScript, esta vez usando CSS para los estilos, usé LocalStorage y custom kooks para separar la lógica de los componentes.",
-    img: "/images/tienda-guitarras.webp",
-    url: "https://magnificent-salmiakki-3ed837.netlify.app/",
-    technologies: ["React", "CSS", "TypeScript"],
-  },
-];
+ */
